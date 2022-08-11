a68d796d8f448578b0a3e55991f372ef29f07eb5
Limbo89 <gmd_2006@mail.ru>
2022-08-11 20:00:33 +0700

    11.08.22

---
+++ 11.08.22/scripts/script.js
@@ -0,0 +1,11 @@
+let cards = [
+    {
+        "name":"поварёшка",
+        "description":"Это 1 товар",
+        "img":"images/",
+        "price":123456,
+        "date_of_create":"21.02.11",
+        "favorite":0,
+    },
+];
+// Создатель, разработчик
