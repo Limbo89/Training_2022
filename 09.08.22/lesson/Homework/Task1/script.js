eb3326ee2ba70d78829ec219a66ec47bf5610cbe
Limbo89 <gmd_2006@mail.ru>
2022-08-15 23:04:49 +0700

    15.08.22

---
+++ 09.08.22/lesson/Homework/Task1/script.js
@@ -0,0 +1,30 @@
+// cards = [{
+//     content: {
+//         title: "Test 1",
+//         description: "test",
+//     },
+//     description: { text: "testtest" }
+// },
+// {
+//     content: {
+//         title: "Test 2",
+//         description: "test",
+//     },
+//     description: { text: "test2test" }
+// }];
+
+// newCard = {
+//     content: {
+//         title: "123",
+//         description: "123",
+//     },
+//     description: { text: "123" }
+// };
+// // Решение без рекурсии
+// // let clone = JSON.parse(JSON.stringify(newCard));
+// function assignNewCard(card) {
+//     cards.push(card);
+// }
+// assignNewCard(clone);
+// newCard.content.title = 'test3';
+// console.log(cards);
