9561a725171987876892ced30a585a2ba5b1de5d
Limbo89 <gmd_2006@mail.ru>
2022-08-16 18:46:46 +0700

    123

--- 09.08.22/lesson/Homework/Task1/script.js
+++ 09.08.22/lesson/Homework/Task1/script.js
@@ -1,30 +1,30 @@
-// cards = [{
-//     content: {
-//         title: "Test 1",
-//         description: "test",
-//     },
-//     description: { text: "testtest" }
-// },
-// {
-//     content: {
-//         title: "Test 2",
-//         description: "test",
-//     },
-//     description: { text: "test2test" }
-// }];
+cards = [{
+    content: {
+        title: "Test 1",
+        description: "test",
+    },
+    description: { text: "testtest" }
+},
+{
+    content: {
+        title: "Test 2",
+        description: "test",
+    },
+    description: { text: "test2test" }
+}];
 
-// newCard = {
-//     content: {
-//         title: "123",
-//         description: "123",
-//     },
-//     description: { text: "123" }
-// };
-// // Решение без рекурсии
-// // let clone = JSON.parse(JSON.stringify(newCard));
-// function assignNewCard(card) {
-//     cards.push(card);
-// }
-// assignNewCard(clone);
-// newCard.content.title = 'test3';
-// console.log(cards);
+newCard = {
+    content: {
+        title: "123",
+        description: "123",
+    },
+    description: { text: "123" }
+};
+// Решение без рекурсии
+function assignNewCard(card) {
+    let newCard = JSON.parse(JSON.stringify(newCard));
+    cards.push(newCard);
+}
+assignNewCard(clone);
+newCard.content.title = 'test3';
+console.log(cards);
