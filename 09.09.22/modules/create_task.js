dcda3d64251f1de728f1345eb7a7529b7b5f2f69
Limbo89 <gmd_2006@mail.ru>
2022-09-17 16:17:26 +0700

    17.09.22

--- 09.09.22/modules/create_task.js
+++ 09.09.22/modules/create_task.js
@@ -1,5 +1,47 @@
-function task(title_, description_){
-    this.title = title_;
-    this.description = description_;
+let id = 0;
+function Card (title_, description_, status_) {
+    this.id = id++;
+    this.properties = {
+        title:title_,
+        description:description_,
+        status:status_,
+    }
+    this.create = `
+    <div class="task">
+        <span class="taskname">${this.properties.title}</span> <br> <br>
+        <span class="tasktext">${this.properties.description}</span>
+        <div class="cross">
+            <img src="./images/Group_2.png" alt="">
+        </div>
+        <div class="tick">
+            <img src="./images/Vector_5.png" alt="">
+        </div>
+    </div>`;
+    this.delete = this.id;
+    this.update = ``;
 }
-export { task }
+
+function TaskCards () {
+    this.cards = [];
+    this.pushCard = (card) => {
+        this.cards.push(card);
+    }
+    this.view_cards = (сurrent_div, complete_div) => {
+        сurrent_div.innerHTML = `<div class="create_task"><img src="./images/pngegg.png" alt=""></div>`;
+        complete_div.innerHTML = '';
+        for (let card of this.cards){
+            if(card.properties.status == false){
+                сurrent_div.innerHTML += `${card.create}`
+            } else {
+                complete_div.innerHTML += `${card.create}`
+            }
+        }
+    }
+    this.delete_card = () => {
+
+    }
+    // свойство со всеми карточками
+    // свойство с выполненными карточками
+    // свойство для удаления/добавления/выполнения карточки
+}
+export { Card, TaskCards }
--- 09.09.22/script.js
+++ 09.09.22/script.js
@@ -1,9 +1,55 @@
-import * as drag_module from "modules/drag.js";
+import * as drag_module from "./modules/drag.js";
 import * as task_module from "./modules/create_task.js";
+let card;
+let cards = new task_module.TaskCards();
+let popUp = document.querySelector(".popUp");
+let div_current_tasks = document.querySelector(".current");
+let div_complete_tasks = document.querySelector(".complete");
+// function view_items() {
+//     div_current_tasks.innerHTML = '<div class="create_task"><img src="./images/pngegg.png" alt=""></div>';
+//     div_complete_tasks.innerHTML = '';
+//     complete_tasks.forEach((task) => {
+//         div_complete_tasks.innerHTML += `
+//         <div class="task">
+//             <span class="taskname">${task.title}</span> <br> <br>
+//             <span class="tasktext">${task.description}</span>
+//             <div class="cross">
+//                 <img src="./images/Group_2.png" alt="">
+//             </div>
+//             <div class="tick">
+//                 <img src="./images/Vector_5.png" alt="">
+//             </div>
+//         </div>
+//     `;
+//     });
+//     current_tasks.forEach((task) => {
+//         div_current_tasks.innerHTML += `
+//         <div class="task">
+//         <span class="taskname">${task.title}</span> <br> <br>
+//         <span class="tasktext">${task.description}</span>
+//         <div class="cross">
+//             <img src="./images/Group_2.png" alt="">
+//         </div>
+//         <div class="tick">
+//             <img src="./images/Vector_5.png" alt="">
+//         </div>
+//     </div>
+//     `;
+document.querySelector(".create_task").addEventListener("click", () => {
+    popUp.classList.toggle('active');
+});
 
-let card = task_module.task("title123", "udgasfhoasdfg");
-console.log(card);
-
+document.querySelector(".popUp_button").addEventListener("click", () => {
+    let title = document.querySelector(".lable").value;
+    let description = document.querySelector(".description").value;
+    cards.pushCard(card = new task_module.Card(title, description, false));
+    cards.pushCard(card = new task_module.Card(title, description, true));
+    cards.view_cards(div_current_tasks, div_complete_tasks);
+    console.log(cards);
+});
+document.querySelector(".close_popUp").addEventListener("click", () => {
+    popUp.classList.toggle('active');
+});
 // var balls = document.querySelectorAll('#ball');
 // for (let ball of balls){
 //     drag_module.drag(ball)
