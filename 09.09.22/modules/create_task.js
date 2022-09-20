4ba698499960634ae9361cbc8f22d16e618410ce
Limbo89 <gmd_2006@mail.ru>
2022-09-20 18:30:50 +0700

    20.09.22

--- 09.09.22/modules/create_task.js
+++ 09.09.22/modules/create_task.js
@@ -1,10 +1,10 @@
 let id = 0;
-function Card (title_, description_, status_) {
+function Card(title_, description_, status_) {
     this.id = id++;
     this.properties = {
-        title:title_,
-        description:description_,
-        status:status_,
+        title: title_,
+        description: description_,
+        status: status_,
     }
     this.create = `
     <div class="task">
@@ -13,35 +13,49 @@ function Card (title_, description_, status_) {
         <div class="cross">
             <img src="./images/Group_2.png" alt="">
         </div>
+        <input type="text" class="id" value="${this.id}">
         <div class="tick">
             <img src="./images/Vector_5.png" alt="">
         </div>
     </div>`;
-    this.delete = this.id;
     this.update = ``;
 }
 
-function TaskCards () {
+function TaskCards() {
     this.cards = [];
     this.pushCard = (card) => {
         this.cards.push(card);
     }
     this.view_cards = (сurrent_div, complete_div) => {
-        сurrent_div.innerHTML = `<div class="create_task"><img src="./images/pngegg.png" alt=""></div>`;
+        сurrent_div.innerHTML = `
+        <div class="create_task">
+            <img src="./images/pngegg.png" alt="">
+        </div>`;
         complete_div.innerHTML = '';
-        for (let card of this.cards){
-            if(card.properties.status == false){
+        for (let card of this.cards) {
+            if (card.properties.status == false) {
                 сurrent_div.innerHTML += `${card.create}`
             } else {
                 complete_div.innerHTML += `${card.create}`
             }
         }
     }
-    this.delete_card = () => {
-
+    this.delete_card = (id) => {
+        let clone = JSON.parse(JSON.stringify(this.cards));
+        this.cards.forEach(function (card, index) {
+            if (id == card.id) {
+                clone.splice(index, 1);
+            }
+        })
+        this.cards = JSON.parse(JSON.stringify(clone));
     }
-    // свойство со всеми карточками
-    // свойство с выполненными карточками
+    this.complete_card = (id) => {
+        this.cards.forEach(function (card) {
+            if (id == card.id) {
+                card.properties.status = true;
+            }
+        })
+    }
     // свойство для удаления/добавления/выполнения карточки
 }
 export { Card, TaskCards }
--- 09.09.22/script.js
+++ 09.09.22/script.js
@@ -1,59 +1,58 @@
 import * as drag_module from "./modules/drag.js";
 import * as task_module from "./modules/create_task.js";
 let card;
+let crosses;
+let ticks;
 let cards = new task_module.TaskCards();
 let popUp = document.querySelector(".popUp");
 let div_current_tasks = document.querySelector(".current");
 let div_complete_tasks = document.querySelector(".complete");
-// function view_items() {
-//     div_current_tasks.innerHTML = '<div class="create_task"><img src="./images/pngegg.png" alt=""></div>';
-//     div_complete_tasks.innerHTML = '';
-//     complete_tasks.forEach((task) => {
-//         div_complete_tasks.innerHTML += `
-//         <div class="task">
-//             <span class="taskname">${task.title}</span> <br> <br>
-//             <span class="tasktext">${task.description}</span>
-//             <div class="cross">
-//                 <img src="./images/Group_2.png" alt="">
-//             </div>
-//             <div class="tick">
-//                 <img src="./images/Vector_5.png" alt="">
-//             </div>
-//         </div>
-//     `;
-//     });
-//     current_tasks.forEach((task) => {
-//         div_current_tasks.innerHTML += `
-//         <div class="task">
-//         <span class="taskname">${task.title}</span> <br> <br>
-//         <span class="tasktext">${task.description}</span>
-//         <div class="cross">
-//             <img src="./images/Group_2.png" alt="">
-//         </div>
-//         <div class="tick">
-//             <img src="./images/Vector_5.png" alt="">
-//         </div>
-//     </div>
-//     `;
-document.querySelector(".create_task").addEventListener("click", () => {
-    popUp.classList.toggle('active');
-});
-
 document.querySelector(".popUp_button").addEventListener("click", () => {
     let title = document.querySelector(".lable").value;
     let description = document.querySelector(".description").value;
     cards.pushCard(card = new task_module.Card(title, description, false));
-    cards.pushCard(card = new task_module.Card(title, description, true));
-    cards.view_cards(div_current_tasks, div_complete_tasks);
-    console.log(cards);
+    renderPage()
 });
-document.querySelector(".close_popUp").addEventListener("click", () => {
+function renderPage() {
+    cards.view_cards(div_current_tasks, div_complete_tasks);
+    let create_task = document.querySelector(".create_task");
+    create_task.addEventListener("click", () => {
+        popUp.classList.toggle('active');
+    });
+    crosses = document.querySelectorAll(".cross");
+    ticks = document.querySelectorAll(".tick");
+    addEvent(crosses, ticks);
+    let tasks = document.querySelectorAll('.task');
+    for (let task of tasks){
+        // drag_module.drag(task)
+        task.addEventListener("mousedown", ()=>{
+            drag_module.drag(task);
+        });
+    }
+}
+function addEvent(crosses, ticks) {
+    if (crosses.length > 0) {
+        for (let cross of crosses) {
+            cross.addEventListener("click", (e) => {
+                let id = Number(e.target.parentNode.parentNode.children[5].value);
+                cards.delete_card(id);
+                renderPage();
+            })
+        }
+    }
+    if (ticks.length > 0) {
+        for (let tick of ticks) {
+            tick.addEventListener("click", (e) => {
+                let id = Number(e.target.parentNode.parentNode.children[5].value);
+                cards.complete_card(id);
+                renderPage();
+            })
+        }
+    }
+
+}
+document.querySelector(".close_popUp").addEventListener("click", (e) => {
+    // console.log(e.target.parentNode);
     popUp.classList.toggle('active');
 });
-// var balls = document.querySelectorAll('#ball');
-// for (let ball of balls){
-//     drag_module.drag(ball)
-//     ball.addEventListener("mousedown", ()=>{
-//         drag_module.drag(ball);
-//     });
-// }
+renderPage()
